b125a33b31d8b58fbd030483ed204b79f5b467c8
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2024-04-06 10:48:01 +0300

    feat(blockchain): blockchain to represent a chain

--- internal/blockchain/blockchain.go
+++ internal/blockchain/blockchain.go
@@ -1,1 +1,26 @@
 package blockchain
+
+import "github.com/brianlusina/gochain/internal/block"
+
+// BlockChain represents the block chain
+type BlockChain struct {
+	blocks []*block.Block
+}
+
+// BlockChainParams represents a param struct to create a block chain
+type BlockChainParams struct {
+	// Blocks is a slice of blocks
+	Blocks []*block.Block
+}
+
+// New creates a new block chain
+func New(params BlockChainParams) *BlockChain {
+	return &BlockChain{
+		blocks: params.Blocks,
+	}
+}
+
+// Blocks returns a slice of blocks in this chain
+func (chain *BlockChain) Blocks() []*block.Block {
+	return chain.blocks
+}
