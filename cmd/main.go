582d96fa69fb0852ef5eb99b94f038cd9325e7c8
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2024-04-08 14:24:48 +0300

    chore(main): test out initializing a block chain

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/brianlusina/gochain/internal/block"
+	"github.com/brianlusina/gochain/internal/blockchain"
 )
 
 func main() {
-	fmt.Println("Blockchain!")
+	genesisBlock := block.CreateBlock("Genesis", "")
+
+	chain := blockchain.New(blockchain.BlockChainParams[string]{
+		Blocks: []*block.Block[string]{genesisBlock},
+	})
+
+	chain.AddBlock("Block 1")
+	chain.AddBlock("Block 2")
+	chain.AddBlock("Block 3")
+
+	for _, block := range chain.Blocks() {
+		fmt.Printf("Previous hash: %x\n", block.PrevHash())
+		fmt.Printf("Data in Block: %s\n", block.Data())
+		fmt.Printf("Hash of block: %x\n", block.Hash())
+		fmt.Println()
+	}
 }
